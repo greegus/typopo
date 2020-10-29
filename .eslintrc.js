9ed8cc28f94b39ca3d8261b666f35c60abe39837
Matúš Duchoň <matus.duchon@gmail.com>
2020-10-30 00:06:48 +0100

    Remove babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 2020,
 		sourceType: 'module',
-		parser: 'babel-eslint',
+		parser: '@typescript-eslint/parser'
 	},
 	plugins: ['prettier'],
 	ignorePatterns: ['dist/*']
